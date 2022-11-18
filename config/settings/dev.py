2463ab8f0f42a094dbcf0daab5e00acb7417212f
Eduardo <eduardo_sdacr@hotmail.com>
2022-11-17 19:17:24 -0500

    Primera configuración

---
+++ config/settings/dev.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+#Using PostGreSQL
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
+
+'''
+#Using SQLite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
